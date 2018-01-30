bce157395cf7793c574a0539d196c1b995eea2d5
Chris <32649648+chrfa37@users.noreply.github.com>
2018-01-30 13:42:38 +0200

    Finished ImageCropper class

--- src/general/ImageCropper.java
+++ src/general/ImageCropper.java
@@ -1,17 +1,98 @@
 package general;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ImageCropper {
 
-	Image img;
+	final Image originalImage;
+	final double imgHeight;
+	final double imgWidth;
 	
-	public ImageCropper(Image img) {
-		this.img = img;
+	int rows;
+	int columns;
+	int pieces;
+	ImageView[] imagePieces;
+	
+	
+	//Constructors
+	public ImageCropper(Image originalImage) {
+		this.originalImage = originalImage;
+		imgHeight = originalImage.getHeight();
+		imgWidth = originalImage.getWidth();
+		setRows(1);
+		setColumns(1);
+		setPieces();
 	}
 	
-	public Image[] crop(int pieces) {
-		Image[] imagePieces = new Image[pieces];
+	public ImageCropper(Image originalImage, int rows, int columns) {
+		this.originalImage = originalImage;
+		imgHeight = originalImage.getHeight();
+		imgWidth = originalImage.getWidth();
+		setRows(rows);
+		setColumns(columns);
+		setPieces();
+	}
+	
+	//crop method
+	public void crop() {
+		imagePieces = new ImageView[pieces];
+		if (pieces == 1) {
+			imagePieces[0].setImage(originalImage);			
+		} else {
+			int counter = 0;
+			double pieceHeight = imgHeight / rows;
+			double pieceWidth = imgWidth / columns;
+			for (int i = 1; i <= rows; i++) {
+				for (int j = 1; j <= columns; j++) {
+					imagePieces[counter] = new ImageView();
+					imagePieces[counter].setImage(originalImage);
+					Rectangle2D viewportRect = new Rectangle2D(pieceWidth * (j - 1), pieceHeight * (i - 1), pieceWidth, pieceHeight);
+					imagePieces[counter].setViewport(viewportRect);
+					counter++;
+				}
+			}			
+		}
+	}
+	
+	
+	//Getters and Setters
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		if (rows <= 0) {
+			rows = 1;
+		}
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		if (columns <= 0) {
+			columns = 1;
+		}
+		this.columns = columns;
+	}
+
+	public int getPieces() {
+		return pieces;
+	}
+
+	private void setPieces() {
+		pieces = rows * columns;
+	}
+
+	public Image getOriginalImage() {
+		return originalImage;
+	}
+
+	public ImageView[] getImagePieces() {
 		return imagePieces;
 	}
 	
